c8455bf2989fd7c80161e32990c3bb515961c10c
Iulian Roman <iulian.roman@cognizant.com>
2026-03-09 17:07:10 +0200

    feat: added sqlalchemy, synced with trainer's code

---
+++ db/database.py
@@ -0,0 +1,31 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import sessionmaker
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+def create_sql_lite_engine(path: str | None = None):
+    # if path is None:
+    #     path = settings.sqlite_database_url
+
+    # if not path:
+    #     raise ValueError("SQLITE_database_url environment variable is not set")
+
+    return create_engine(f"sqlite:///{path}")
+
+
+engine = create_sql_lite_engine("chatbox.db")
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    if Session is None:
+        raise ValueError("Database not configured.")
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ db/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, DateTime, Integer, String, func
+from database import Base
+
+
+class UserRecord(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, nullable=False)
+    name = Column(String, nullable=True)
+    phone = Column(String, nullable=True)
+    avatar_url = Column(String, nullable=True)
+    password_hash = Column(String, nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
