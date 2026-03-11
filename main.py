b48049e71091cd49552d6f7dcb3f55476d608cac
Iulian Roman <iulian.roman@cognizant.com>
2026-03-11 15:25:14 +0200

    feat: added lifespan handler, moved models in root dir, updated imports

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-from api.routes import users
-from db.database import engine
-from db.models import Base
+from api.lifespan import lifespan
+from api.routes import users, auth, core
 
-Base.metadata.create_all(bind=engine)
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
+app.include_router(core.router)
+app.include_router(auth.router)
 app.include_router(users.router)
