b6a65a4da1cd063cb5b0d08503f2ac260e903a15
Iulian Roman <iulian.roman@cognizant.com>
2026-03-11 17:49:40 +0200

    added auth route + updated model validation with Field() from pydantic

---
+++ api/routes/auth.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, status, Depends
+from models.user import UserResponse, UserCreate
+from sqlalchemy.orm import Session
+from db.database import get_db
+from api.services import user as user_service
+
+router = APIRouter(prefix="/auth", tags=["auth"])
+
+
+@router.post("/login")
+def login():
+    pass
+
+
+@router.post(
+    "/signup", response_model=UserResponse, status_code=status.HTTP_201_CREATED
+)
+def signup(user_create: UserCreate, db: Session = Depends(get_db)):
+    return user_service.create_user(db, user_create)
